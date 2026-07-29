84cbcd448f432e1a24c92918f59e033c4b0be40c
Ganesh Machale <ganeshmachale2006@gmail.com>
2026-07-29 22:08:42 +0530

    insertMany() operation implemented

--- index.js
+++ index.js
@@ -20,8 +20,18 @@ const mongoose = require('mongoose');
 
      const User = mongoose.model("User",userSchema);
    
-      const user1 = new User({name:"Ganesh",email:"ganesh@gmail.com",age:19});
-          user1.save();
-      const user2 = new User({name:"Aditya",email:"Aditya@gmail.com",age:20});
-          user2.save();
+    //   const user1 = new User({name:"Ganesh",email:"ganesh@gmail.com",age:19});
+    //       user1.save();
+    //   const user2 = new User({name:"Aditya",email:"Aditya@gmail.com",age:20});
+    //       user2.save();
      
+     
+      User.insertMany([
+        {name:"Suraj",email:"Suraj@gmail.com", age:21},
+        {name:"vedant",email:"Vedant@gmail.com", age:22},
+        {name:"Pranav",email:"Pranav@gmail.com", age:24}
+      ]).then((res)=>{
+        console.log(res);
+      }).catch((err)=>{
+        console.log(err);
+      })
